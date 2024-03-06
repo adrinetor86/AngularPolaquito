844bae84425c2f4222ba232d716592043ce33df3
Adrian Jacek <adrianjacek@iesluisbraille.es>
2024-03-06 11:01:43 +0100

    dw

--- src/app/polaco/polaco.component.ts
+++ src/app/polaco/polaco.component.ts
@@ -9,13 +9,15 @@ import { Polacothings } from '../polacothings';
   standalone: true,
   imports: [CommonModule,PolandThingsComponent],
   template: `
+    <form>
+      <input type="text" placeholder="Filter by object">
+      <button class="primary" type="button">Search</button>
+
+
+    </form>
     <section>
-      <form>
-        <input type="text" placeholder="Filter by object">
-        <button class="primary" type="button">Search</button>
 
 
-      </form>
       <app-poland-things
         *ngFor="let polacothings of PolacothingsList"
         [polacothings]="polacothings" >
@@ -25,89 +27,82 @@ import { Polacothings } from '../polacothings';
 
 
   `,
-  styleUrl: './polaco.component.css'
+  styleUrl: 'polaco.component.css'
 })
 
 export class PolacoComponent {
 
-  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
-  PolacothingsList: Polacothings[] =[
+   PolacothingsList: Polacothings[] =[
     {
       id: 0,
-      name: 'Acme Fresh Start Housing',
-      power: 'Chicago',
-      photo: `${this.baseUrl}/bernard-hermant-CLKGGwIBTaY-unsplash.jpg`,
+      name: 'VODKA',
+      power: '150',
+      photo: 'https://licorcash.es/cdn/shop/products/MH026_belvedere-vodka_6000_mit-licht_1024x1024@2x.jpg?v=1620825577',
     },
     {
       id: 1,
-      name: 'A113 Transitional Housing',
-      power: 'Santa Monica',
-
-      photo: `${this.baseUrl}/brandon-griggs-wR11KBaB86U-unsplash.jpg`,
+      name: 'BOBR',
+      power: '99',
+      photo: `../assets/bobr.png`,
 
     },
     {
       id: 2,
-      name: 'Warm Beds Housing Support',
-      power: 'Juneau',
-
-      photo: `${this.baseUrl}/i-do-nothing-but-love-lAyXdl1-Wmc-unsplash.jpg`,
+      name: 'AUDI',
+      power: '80',
+      photo: `https://cdn.classicdigest.com/live/carimg/133001_133100/133017_24c324fd51a29f0d.jpg`,
 
     },
     {
       id: 3,
-      name: 'Homesteady Housing',
-      power: 'Chicago',
-
-      photo: `${this.baseUrl}/ian-macdonald-W8z6aiwfi1E-unsplash.jpg`,
+      name: 'PIEROGIS',
+      power: '250',
+      photo: `https://imag.bonviveur.com/pierogi.webp`,
 
     },
     {
       id: 4,
-      name: 'Happy Homes Group',
-      power: 'Gary',
-
-      photo: `${this.baseUrl}/krzysztof-hepner-978RAXoXnH4-unsplash.jpg`,
+      name: 'CASA',
+      power: '20',
+      photo: `https://mymodernmet.com/wp/wp-content/uploads/2017/01/Zalipie-Poland-Painted-Houses-10.jpg`,
 
     },
     {
       id: 5,
-      name: 'Hopeful Apartment Group',
-      power: 'Oakland',
-
-      photo: `${this.baseUrl}/r-architecture-JvQ0Q5IkeMM-unsplash.jpg`,
+      name: 'PIWO',
+      power: '100',
+      photo: `https://res.cloudinary.com/dj484tw6k/f_auto,q_auto,c_pad,b_white,w_524,h_524/v1696459926/42407.png`,
 
     },
     {
       id: 6,
-      name: 'Seriously Safe Towns',
-      power: 'Oakland',
-
-      photo: `${this.baseUrl}/phil-hearing-IYfp2Ixe9nM-unsplash.jpg`,
+      name: 'LEWANDOWSKI',
+      power: '70',
+      photo: `https://assets.laliga.com/squad/2023/t178/p56764/256x278/p56764_t178_2023_1_001_000.png`,
 
     },
     {
       id: 7,
-      name: 'Hopeful Housing Solutions',
-      power: 'Oakland',
+      name: 'LOTTO',
+      power: '20',
 
-      photo: `${this.baseUrl}/r-architecture-GGupkreKwxA-unsplash.jpg`,
+      photo: `https://www.pap.pl/sites/default/files/styles/main_image/public/202106/39624323_36553092.jpg?itok=T9HqoQPw`,
 
     },
     {
       id: 8,
-      name: 'Seriously Safe Towns',
-      power: 'Oakland',
+      name: 'ZABKA',
+      power: '120',
 
-      photo: `${this.baseUrl}/saru-robert-9rP3mxf8qWI-unsplash.jpg`,
+      photo: `https://notesfrompoland.com/wp-content/uploads/2022/10/sklep-we-wroclawiu-1080x675.png`,
 
     },
     {
       id: 9,
-      name: 'Capital Safe Towns',
-      power: 'Portland',
+      name: 'PIS',
+      power: '10',
 
-      photo: `${this.baseUrl}/webaliser-_TPTXZd9mOo-unsplash.jpg`,
+      photo: `https://pis.org.pl/uploads/cache/news_single/uploads/media/news/0001/36/28f6f84dd6aa9cec23ebb6340324802ec4efb458.jpeg`,
 
     }
   ];
